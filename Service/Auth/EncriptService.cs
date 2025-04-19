3942b63a968b25fdf850e82d3a1cfb3bea97f11a
Willy Christian <1567225@sga.pucminas.br>
2025-04-19 02:15:18 -0300

    feat: adiciona encriptação da senha do usuário e retira a visualização no front (#4)
    * .
    
    * Fix typo and add some improvements

---
+++ Service/Auth/EncriptService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PocWebDevBackend.Models;
+
+namespace PocWebDevBackend.Service.Auth
+{
+    public class EncriptService : IEncriptService
+    {
+        private readonly PasswordHasher<User> _hasher = new();
+
+        public string HashPassword(User user, string password)
+        {
+            return _hasher.HashPassword(user, password);
+        }
+
+        public bool VerifyPassword(User user, string hashedPassword, string givenPassword)
+        {
+            var result = _hasher.VerifyHashedPassword(user, hashedPassword, givenPassword);
+            return result == PasswordVerificationResult.Success;
+        }
+    }
+}
