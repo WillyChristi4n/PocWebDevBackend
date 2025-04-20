6218159136fb7f0366cb2e7e81d4451ff5187f81
WillyChristi4n <1567225@sga.pucminas.br>
2025-04-20 00:07:52 -0300

    Add user validation on routes and improve a few screens

---
+++ Service/Toast/ToastService.cs
@@ -0,0 +1,17 @@
+﻿
+namespace PocWebDevBackend.Service.Toast
+{
+    public class ToastService
+    {
+        public string Message { get; set; }
+        public MessageType Type { get; set; }
+
+        public enum MessageType
+        {
+            Success,
+            Error,
+            Wrarning,
+            Info,
+        }
+    }
+}
